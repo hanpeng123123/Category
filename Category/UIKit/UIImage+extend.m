c8bc8ab58f93b86b64be908cd06c44300a1679a7
hanpeng123123 <137526523@qq.com>
2016-02-20 22:34:51 +0800

    Category
    收集的分类

---
+++ Category/UIKit/UIImage+extend.m
@@ -0,0 +1,271 @@
+//
+//  UIImage-Extensions.m
+//
+//  Created by Hardy Macia on 7/1/09.
+//  Copyright 2009 Catamount Software. All rights reserved.
+//
+#import "UIImage+extend.h"
+#import <objc/runtime.h>
+static const void *CompleteBlockKey = &CompleteBlockKey;
+static const void *FailBlockKey = &FailBlockKey;
+@interface UIImage ()
+@property (nonatomic,copy)  void(^CompleteBlock)();
+
+@property (nonatomic,copy)  void(^FailBlock)();
+
+@end
+
+@implementation UIImage (UIImage_extend)
+
+
+
+
+/**
+ *  图片剪切为圆形
+ *
+ *  @param originalImage 原始图片
+ *
+ *  @return 剪切后的圆形图片
+ */
+-(UIImage *)roundImage{
+    
+    //获取size
+    CGSize size = [self sizeFromImage:self];
+    
+    CGRect rect = (CGRect){CGPointZero,size};
+    
+    //新建一个图片图形上下文
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0.0f);
+    
+    //获取上下文
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    
+    //绘制圆形路径
+    CGContextAddEllipseInRect(ctx, rect);
+    
+    //剪裁上下文
+    CGContextClip(ctx);
+    
+    //绘制图片
+    [self drawInRect:rect];
+    
+    //取出图片
+    UIImage *roundImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    //结束上下文
+    UIGraphicsEndImageContext();
+    
+    return roundImage;
+}
+
+
+-(CGSize)sizeFromImage:(UIImage *)image{
+    
+    CGSize size = image.size;
+    
+    CGFloat wh =MIN(size.width, size.height);
+    
+    return CGSizeMake(wh, wh);
+}
+
+
++ (UIImage *)imageWithColor:(UIColor *)color {
+    CGSize imageSize = CGSizeMake(1, 1);
+    UIGraphicsBeginImageContextWithOptions(imageSize, 0, [UIScreen mainScreen].scale);
+    [color set];
+    UIRectFill(CGRectMake(0, 0, imageSize.width, imageSize.height));
+    UIImage *pressedColorImg = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return pressedColorImg;
+}
+
+//截取部分图像
+- (UIImage *)imageAtRect:(CGRect)rect {
+    
+    CGImageRef subImageRef = CGImageCreateWithImageInRect(self.CGImage, rect);
+	CGRect smallBounds = CGRectMake(0, 0, CGImageGetWidth(subImageRef), CGImageGetHeight(subImageRef));
+	
+    UIGraphicsBeginImageContext(smallBounds.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextDrawImage(context, smallBounds, subImageRef);
+    
+    // 设置图片旋转方向
+    UIImage* smallImage = [UIImage imageWithCGImage:subImageRef scale:1.0f orientation:self.imageOrientation];
+    
+    UIGraphicsEndImageContext();
+    CGImageRelease(subImageRef);
+	
+    return smallImage;
+}
+
+- (UIImage *)imageResize:(CGSize)reSize {
+    
+    UIGraphicsBeginImageContext(CGSizeMake(reSize.width, reSize.height));
+    [self drawInRect:CGRectMake(0, 0, reSize.width, reSize.height)];
+    UIImage *reSizeImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return reSizeImage;
+}
+
+//等比例缩放
+- (UIImage *)imageToScale:(CGFloat)scale {
+    return [self imageResize:CGSizeMake(self.size.width * scale, self.size.height * scale)];
+}
+
+- (UIImage *)autoScaleWithMaxLength:(CGFloat)maxLength {
+    
+    CGFloat max = MAX(self.size.width, self.size.height);
+    CGFloat scale = 1;
+    
+    if (max > maxLength) {
+        scale = maxLength / max;
+    }
+    
+    return [self imageToScale:scale];
+}
+
+- (BOOL)writeToFile:(NSString *)aPath {
+    if ((self == nil) || (aPath == nil) || ([aPath isEqualToString:@""])) return NO;
+    @try
+    {
+        NSData *imageData = nil;
+        NSString *ext = aPath.pathExtension.lowercaseString;
+        if ([ext isEqualToString:@"png"]) {
+            imageData = UIImagePNGRepresentation(self);
+        } else {
+            // the rest, we write to jpeg
+            // 0. best, 1. lost. about compress.
+            imageData = UIImageJPEGRepresentation(self, 1.0);
+        }
+        if ((imageData == nil) || ([imageData length] <= 0))
+            return NO;
+        [imageData writeToFile:aPath atomically:YES];
+        return YES;
+    }
+    @catch (NSException *e)
+    {
+        NSLog(@"create thumbnail exception.");
+    }
+    return NO;
+}
+
+/**
+ *  保存相册
+ *
+ *  @param completeBlock 成功回调
+ *  @param completeBlock 出错回调
+ */
+-(void)savedPhotosAlbum:(void(^)())completeBlock failBlock:(void(^)())failBlock{
+    
+    UIImageWriteToSavedPhotosAlbum(self, self, @selector(image:didFinishSavingWithError:contextInfo:),NULL);
+    
+    self.CompleteBlock = completeBlock;
+    self.FailBlock = failBlock;
+}
+
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo{
+    
+    if(error == nil){
+        if(self.CompleteBlock != nil) self.CompleteBlock();
+    }else{
+        if(self.FailBlock !=nil) self.FailBlock();
+    }
+    
+}
+
+
+/*
+ *  模拟成员变量
+ */
+-(void (^)())FailBlock{
+    return objc_getAssociatedObject(self, FailBlockKey);
+}
+-(void)setFailBlock:(void (^)())FailBlock{
+    objc_setAssociatedObject(self, FailBlockKey, FailBlock, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+-(void (^)())CompleteBlock{
+    return objc_getAssociatedObject(self, CompleteBlockKey);
+}
+
+-(void)setCompleteBlock:(void (^)())CompleteBlock{
+    objc_setAssociatedObject(self, CompleteBlockKey, CompleteBlock, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (UIImage *)fixOrientation {
+    
+    // No-op if the orientation is already correct
+    if (self.imageOrientation == UIImageOrientationUp) return self;
+    
+    // We need to calculate the proper transformation to make the image upright.
+    // We do it in 2 steps: Rotate if Left/Right/Down, and then flip if Mirrored.
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    
+    switch (self.imageOrientation) {
+        case UIImageOrientationDown:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, self.size.width, self.size.height);
+            transform = CGAffineTransformRotate(transform, M_PI);
+            break;
+            
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+            transform = CGAffineTransformTranslate(transform, self.size.width, 0);
+            transform = CGAffineTransformRotate(transform, M_PI_2);
+            break;
+            
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, 0, self.size.height);
+            transform = CGAffineTransformRotate(transform, -M_PI_2);
+            break;
+        default:
+            break;
+    }
+    
+    switch (self.imageOrientation) {
+        case UIImageOrientationUpMirrored:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, self.size.width, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+            
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, self.size.height, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+        default:
+            break;
+    }
+    
+    // Now we draw the underlying CGImage into a new context, applying the transform
+    // calculated above.
+    CGContextRef ctx = CGBitmapContextCreate(NULL, self.size.width, self.size.height,
+                                             CGImageGetBitsPerComponent(self.CGImage), 0,
+                                             CGImageGetColorSpace(self.CGImage),
+                                             CGImageGetBitmapInfo(self.CGImage));
+    CGContextConcatCTM(ctx, transform);
+    switch (self.imageOrientation) {
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            // Grr...
+            CGContextDrawImage(ctx, CGRectMake(0,0,self.size.height,self.size.width), self.CGImage);
+            break;
+            
+        default:
+            CGContextDrawImage(ctx, CGRectMake(0,0,self.size.width,self.size.height), self.CGImage);
+            break;
+    }
+    
+    // And now we just create a new UIImage from the drawing context
+    CGImageRef cgimg = CGBitmapContextCreateImage(ctx);
+    UIImage *img = [UIImage imageWithCGImage:cgimg];
+    CGContextRelease(ctx);
+    CGImageRelease(cgimg);
+    return img;
+}
+
+@end
---
+++ Category/UIKit/UIView+extend.h
@@ -0,0 +1,111 @@
+//
+//  UIView+extend.h
+//  NSObject+extend
+//
+//  Created by Steven on 14/12/10.
+//  Copyright (c) 2014年 Neva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+/**
+ *  方向类型
+ */
+
+typedef enum{
+    
+    //上
+    UIViewBorderDirectTop=0,
+    
+    //左
+    UIViewBorderDirectLeft,
+    
+    //下
+    UIViewBorderDirectBottom,
+    
+    //右
+    UIViewBorderDirectRight,
+    
+    
+}UIViewBorderDirect;
+@interface UIView (UIView_extend)
+
+@property (nonatomic, assign) CGFloat filletRadius;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGFloat Y;
+@property (nonatomic, assign) CGFloat X;
+@property (assign, nonatomic) CGFloat centerX;
+@property (assign, nonatomic) CGFloat maxY;
+/**
+ *  所在控制器
+ */
+@property(nonatomic,strong,readonly)id controller;
+/**
+ *  中心位置
+ */
+- (void)centerInSuperView;
+/**
+ *  审美位置
+ */
+- (void)centerAestheticInSuperView;
+/**
+ *  清除所有subView
+ */
+-(void)clearAllSubView;
+
+/**
+ *  获取所有subView
+ *
+ *  @return UIView Array
+ */
+-(NSArray *)allSubViews;
+
+/**
+ *  遍历所有subView取消其焦点,收回键盘
+ */
+-(void)resignResponder;
+
+/**
+ *  获取UIView的截图
+ *
+ *  @return UIImage 图片的width和heigh，会乘以 [UIScreen mainScreen].scale
+ */
+- (UIImage *)imageSnapshot;
+
+/**
+ *  限制 UIView 的宽度，计算出autolayout后的高度
+ *
+ */
+- (CGFloat)viewHeightWithLimitWidth:(CGFloat)limitWidth;
+#pragma mark - 调试
+/**
+ *  调试
+ */
+-(void)debugWithColor:(UIColor *)color width:(CGFloat)width;
+
+/**
+ *  自动从xib创建视图
+ */
++(instancetype)viewFromXIB;
+/**
+ *  添加边框：注给scrollView添加会出错
+ *
+ *  @param direct 方向
+ *  @param color  颜色
+ *  @param width  线宽
+ */
+-(void)addSingleBorder:(UIViewBorderDirect)direct color:(UIColor *)color width:(CGFloat)width;
+
+
+#pragma mark  视图形状处理
+/**
+ *  添加边框
+ */
+-(void)setBorder:(UIColor *)color width:(CGFloat)width;
+
+/**
+ *  圆角
+ */
+-(void)setRadius:(CGFloat)r;
+
+@end
